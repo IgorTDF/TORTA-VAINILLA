4f37e7e7006a6714e0d5bf8f1a8959257c15852a
Igor.tdf <104040266+IgorTDF@users.noreply.github.com>
2022-04-26 19:46:21 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+console.log("TORTA VAINILLA");
+console.log();
+console.log("Ingredientes");
+console.log("4 huevos");
+console.log("2 tazas de azúcar");
+console.log("1/2 taza de leche");
+console.log("2 cucharaditas de polvo de hornear");
+console.log("1 pizca de sal");
+console.log("1 cucharita de vainilla");
+console.log();
+console.log("Elaboracion");
+console.log("1 Separar las claras de las yemas de los huevos.");
+console.log("2 Batir las yemas con 1 taza de azúcar.");
+console.log("3 Entibiar (no calentar demasiado!) la leche y agregarla");
+console.log(
+  "4 Seguir batiendo y agregar la harina, el polvo para hornear y sal."
+);
+console.log(
+  "5 En otro recipiente batir las claras a nieve y agregar la otra taza de azúcar."
+);
+console.log("6 Agregar la vainilla a las claras a nieve.");
+console.log(
+  "7 Incorporar con movimientos envolventes las claras a nieve a la mezcla de yemas."
+);
+console.log("8 Enmantecar y enharinar un molde para horno.");
+console.log("9 Verter la preparación en el molde.");
+console.log(
+  "10 Llevar la Torta de vainilla a horno moderado durante 20 minutos aproximadamente, o hasta que al introducir un palito este salga seco."
+);
